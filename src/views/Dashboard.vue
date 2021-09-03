541c630a4dcdf678493750f4d991e9722d21d58f
Alex Burdoy <alex.burdoy@somosinapsis.com>
2021-09-03 12:07:46 +0200

    First commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard">
+    <h1>Dashboard View</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8000";
+
+export default {
+  data() {
+    return {
+      email: "",
+    };
+  },
+
+  mounted() {
+    axios.get("/apicustomers").then((response) => {
+      this.email = response.data.email;
+    });
+  },
+};
+</script>
